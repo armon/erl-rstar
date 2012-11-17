36f11ca9d5f401affc832d4559fa51663a4584c7
Armon Dadgar <armon.dadgar@gmail.com>
2012-11-16 16:56:25 -0800

    Working on interface design

---
+++ src/rstar.erl
@@ -0,0 +1,37 @@
+-module(rstar).
+-export([new/0, insert/2, delete/2, searchWithin/2, searchNearest/3, searchAround/3]).
+-export_type([rtree/0, geometry/0]).
+
+% Expose opaque type references
+-opaque rtree() :: any().
+-opaque geometry() :: any().
+
+% Returns a new empty R* tree
+-spec new() -> rtree().
+new() -> ok.
+
+% Inserts a new geometric point into the R* tree and
+% returns a new tree
+-spec insert(rtree(), geometry()) -> rtree().
+insert(_Tree, _Geometry) -> ok.
+
+% Removes a geometric point from the R* tree and
+% returns a new tree
+-spec delete(rtree(), geometry()) -> rtree().
+delete(_Tree, _Geometry) -> ok.
+
+% Searches for the geometries contained or intersecting
+% the given geometry
+-spec searchWithin(rtree(), geometry()) -> [geometry()].
+searchWithin(_Tree, _Geometry) -> ok.
+
+% Searches for the K-nearest neighbors to the given
+% geometry.
+-spec searchNearest(rtree(), geometry(), integer()) -> [geometry()].
+searchNearest(_Tree, _Geometry, _K) -> ok.
+
+% Searches for the geometries contained or intersecting
+% the given geometry
+-spec searchAround(rtree(), geometry(), float()) -> [geometry()].
+searchAround(_Tree, _Geometry, _Distance) -> ok.
+
