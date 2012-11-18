6edfcf6bd9104be3177a7db6fd16e63c62e11093
Armon Dadgar <armon.dadgar@gmail.com>
2012-11-18 13:23:23 -0800

    Adding dimensionality checks to rstar interface

--- src/rstar.erl
+++ src/rstar.erl
@@ -18,31 +18,47 @@ new(Dimensions) -> #rtree{dimensions=Dimensions}.
 % Inserts a new geometric point into the R* tree and
 % returns a new tree
 -spec insert(rtree(), geometry()) -> rtree() | {error, dimensionality}.
-insert(#rtree{dimensions=TD}, #geometry{dimensions=GD}) when TD =/= GD -> {error, dimensionality};
+insert(#rtree{dimensions=TD}, #geometry{dimensions=GD}) when TD =/= GD ->
+    {error, dimensionality};
+
 insert(_Tree, _Geometry) -> ok.
 
 
 % Removes a geometric point from the R* tree and
 % returns a new tree
 -spec delete(rtree(), geometry()) -> rtree().
+delete(#rtree{dimensions=TD}, #geometry{dimensions=GD}) when TD =/= GD ->
+    {error, dimensionality};
+
 delete(_Tree, _Geometry) -> ok.
 
 
 % Searches for the geometries contained or intersecting
 % the given geometry
 -spec search_within(rtree(), geometry()) -> [geometry()].
+search_within(#rtree{dimensions=TD}, #geometry{dimensions=GD}) when TD =/= GD ->
+    {error, dimensionality};
+
 search_within(_Tree, _Geometry) -> ok.
 
 
 % Searches for the K-nearest neighbors to the given
 % geometry.
 -spec search_nearest(rtree(), geometry(), integer()) -> [geometry()].
+search_nearest(#rtree{dimensions=TD}, #geometry{dimensions=GD}, _) when TD =/= GD ->
+    {error, dimensionality};
+search_nearest(_, _, Nearest) when Nearest =< 0 ->
+    {error, badarg};
+
 search_nearest(_Tree, _Geometry, _K) -> ok.
 
 
 % Searches for the geometries contained or intersecting
 % the given geometry
 -spec search_around(rtree(), geometry(), float()) -> [geometry()].
+search_around(#rtree{dimensions=TD}, #geometry{dimensions=GD}, _) when TD =/= GD ->
+    {error, dimensionality};
+
 search_around(_Tree, _Geometry, _Distance) -> ok.
 
 
