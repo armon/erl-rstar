953af840fd8a0ceab4cdd33af096c873f9d00778
Armon Dadgar <armon.dadgar@gmail.com>
2012-11-27 15:45:48 -0800

    Testing delete_internal

--- test/rstar_delete_test.erl
+++ test/rstar_delete_test.erl
@@ -285,9 +285,99 @@ delete_recursive_parent_root_underflow_test(_) ->
         end
     ).
 
-delete_internal_no_underflow_test(_) -> ok.
-delete_internal_single_child_test(_) -> ok.
-delete_internal_reinsert_test(_) -> ok.
+delete_internal_no_underflow_test(_) ->
+    ?_test(
+        begin
+            L1 = #geometry{dimensions=2, mbr=[{1,1}, {1,1}], value=#leaf{}},
+            L2 = #geometry{dimensions=2, mbr=[{0,0}, {0,0}], value=#leaf{}},
+            L3 = #geometry{dimensions=2, mbr=[{2, 2}, {2, 2}], value=#leaf{}},
+            L4 = #geometry{dimensions=2, mbr=[{-1, -1}, {-1, -1}], value=#leaf{}},
+
+            NGeo = rstar_geometry:bounding_box([L3, L2, L1]),
+            N1 = NGeo#geometry{value=#leaf{entries=[L3, L2, L1]}},
+
+            N2Geo = rstar_geometry:bounding_box([L4, L3, L2, L1]),
+            N2 = N2Geo#geometry{value=#leaf{entries=[L4, L3, L2, L1]}},
+
+            N3Geo = rstar_geometry:bounding_box([L4, L3, L1]),
+            N3 = N3Geo#geometry{value=#leaf{entries=[L4, L3, L1]}},
+
+            RootGeo = rstar_geometry:bounding_box([N1, N2, N3]),
+            Root = RootGeo#geometry{value=#node{children=[N1, N2, N3]}},
+
+            Params1 = #rt_params{min=2, max=5, reinsert=2},
+
+            % Create the expected root
+            ExpectN1Geo = rstar_geometry:bounding_box([L3, L1]),
+            ExpectN1 = ExpectN1Geo#geometry{value=#leaf{entries=[L3, L1]}},
+
+            ExpectGeo = rstar_geometry:bounding_box([ExpectN1, N2, N3]),
+            ExpectRoot = ExpectGeo#geometry{value=#node{children=[ExpectN1, N2, N3]}},
+
+            % No underflow expected
+            ?assertEqual(ExpectRoot, rstar_delete:delete_internal(Params1, [Root, N1], L2))
+        end
+    ).
+
+delete_internal_single_child_test(_) ->
+    ?_test(
+        begin
+            L1 = #geometry{dimensions=2, mbr=[{1,1}, {1,1}], value=#leaf{}},
+            L2 = #geometry{dimensions=2, mbr=[{0,0}, {0,0}], value=#leaf{}},
+            L3 = #geometry{dimensions=2, mbr=[{2, 2}, {2, 2}], value=#leaf{}},
+            L4 = #geometry{dimensions=2, mbr=[{-1, -1}, {-1, -1}], value=#leaf{}},
+
+            NGeo = rstar_geometry:bounding_box([L2]),
+            N1 = NGeo#geometry{value=#leaf{entries=[L2]}},
+
+            N2Geo = rstar_geometry:bounding_box([L4, L3, L2, L1]),
+            N2 = N2Geo#geometry{value=#leaf{entries=[L4, L3, L2, L1]}},
+
+            RootGeo = rstar_geometry:bounding_box([N1, N2]),
+            Root = RootGeo#geometry{value=#node{children=[N1, N2]}},
+
+            Params1 = #rt_params{min=2, max=5, reinsert=2},
+
+            ?assertEqual(N2, rstar_delete:delete_internal(Params1, [Root, N1], L2))
+        end
+    ).
+
+delete_internal_reinsert_test(_) ->
+    ?_test(
+        begin
+            L1 = #geometry{dimensions=2, mbr=[{1,1}, {1,1}], value=#leaf{}},
+            L2 = #geometry{dimensions=2, mbr=[{0,0}, {0,0}], value=#leaf{}},
+            L3 = #geometry{dimensions=2, mbr=[{2, 2}, {2, 2}], value=#leaf{}},
+            L4 = #geometry{dimensions=2, mbr=[{-1, -1}, {-1, -1}], value=#leaf{}},
+            L5 = #geometry{dimensions=2, mbr=[{3, 3}, {3, 3}], value=#leaf{}},
+            L6 = #geometry{dimensions=2, mbr=[{4, 4}, {4, 4}], value=#leaf{}},
+
+            NGeo = rstar_geometry:bounding_box([L2, L1]),
+            N1 = NGeo#geometry{value=#leaf{entries=[L2, L1]}},
+
+            N2Geo = rstar_geometry:bounding_box([L4, L3]),
+            N2 = N2Geo#geometry{value=#leaf{entries=[L4, L3]}},
+
+            N3Geo = rstar_geometry:bounding_box([L5, L6]),
+            N3 = N3Geo#geometry{value=#leaf{entries=[L5, L6]}},
+
+            RootGeo = rstar_geometry:bounding_box([N1, N2, N3]),
+            Root = RootGeo#geometry{value=#node{children=[N1, N2, N3]}},
+
+            Params1 = #rt_params{min=2, max=5, reinsert=2},
+
+            % We expect N1 will underflow, causing a re-insertion of L1
+            % This should logically go in N2
+            ExpectN2Geo = rstar_geometry:bounding_box([L4, L3, L1]),
+            ExpectN2 = ExpectN2Geo#geometry{value=#leaf{entries=[L1, L4, L3]}},
+
+            ExpectRootGeo = rstar_geometry:bounding_box([ExpectN2, N3]),
+            ExpectRoot = ExpectRootGeo#geometry{value=#node{children=[ExpectN2, N3]}},
+
+            ?assertEqual(ExpectRoot, rstar_delete:delete_internal(Params1, [Root, N1], L2))
+        end
+    ).
+
 delete_not_found_test(_) -> ok.
 delete_found_test(_) -> ok.
 
