02ad9f91066c5b03b5fda8b4b85efc4e81c8afa5
Armon Dadgar <armon.dadgar@gmail.com>
2012-11-27 13:51:53 -0800

    More tests

--- test/rstar_delete_test.erl
+++ test/rstar_delete_test.erl
@@ -9,7 +9,8 @@ main_test_() ->
      fun setup/0,
      fun cleanup/1,
      [
-      fun find_leaf_exists_test/1
+      fun find_leaf_exists_test/1,
+      fun find_leaf_not_exists_test/1
      ]}.
 
 setup() -> ok.
@@ -27,6 +28,23 @@ find_leaf_exists_test(_) ->
             Root = NGeo#geometry{value=#node{children=[N]}},
 
             ?assertEqual({found, [N, Root]}, rstar_delete:find_leaf(Root, L1))
+        end
+    ).
+
+find_leaf_not_exists_test(_) ->
+    ?_test(
+        begin
+            L1 = #geometry{dimensions=2, mbr=[{1,1}, {1,1}], value=#leaf{}},
+            L2 = #geometry{dimensions=2, mbr=[{0,0}, {0,0}], value=#leaf{}},
+            L3 = #geometry{dimensions=2, mbr=[{2, 2}, {2, 2}], value=#leaf{}},
+            L4 = #geometry{dimensions=2, mbr=[{-1, -1}, {-1, -1}], value=#leaf{}},
+            NGeo = rstar_geometry:bounding_box([L4, L3, L2, L1]),
+            N = NGeo#geometry{value=#leaf{entries=[L4, L3, L2, L1]}},
+            Root = NGeo#geometry{value=#node{children=[N]}},
+
+            G = #geometry{dimensions=2, mbr=[{0.5, 0.5}, {0.5, 0.5}], value=#leaf{}},
+
+            ?assertEqual(not_found, rstar_delete:find_leaf(Root, G))
         end
     ).
 
